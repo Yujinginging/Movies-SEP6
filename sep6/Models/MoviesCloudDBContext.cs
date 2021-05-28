793f7b636b42339adf5014791d640b00c3bc9b80
Teakosheen <43374385+Teakosheen@users.noreply.github.com>
2021-05-28 09:04:13 +0200

    DBcontext with cloud instance connections

---
+++ sep6/Models/MoviesCloudDBContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace Movies_sep6.Models
+{
+    public class MoviesCloudDBContext : DbContext
+    {
+        public DbSet<MoviesCloud> MoviesFromCloud { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            options.UseMySQL("server=34.136.229.1;database=Movies;user=root;password=Sep6_1234");
+        }
+    }
+}
