4cb82cc38aecaf7aeb2937180bb600bbeba754f6
Jing <273451@via.dk>
2021-05-31 13:54:58 +0800

    delete not needed component

--- sep6/ClientApp/src/app/app.module.ts
+++ sep6/ClientApp/src/app/app.module.ts
@@ -1,26 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { HttpClientModule } from '@angular/common/http';
 import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { NavMenuComponent } from './nav-menu/nav-menu.component';
 import { HomeComponent } from './home/home.component';
-//import { CounterComponent } from './counter/counter.component';
-//import { MTopListComponent } from './mTopList/mTopList.component';
 import { MovieComponent } from './movie/movie.component';
-//import { DirectorComponent } from './directorTopList/directorTopList.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavMenuComponent,
     HomeComponent,
-    //CounterComponent,
-    //MTopListComponent,
-    MovieComponent,
-    //DirectorComponent
+    MovieComponent
   ],
   imports: [
     BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
@@ -28,10 +22,7 @@ import { MovieComponent } from './movie/movie.component';
     FormsModule,
     RouterModule.forRoot([
       { path: '', component: HomeComponent, pathMatch: 'full' },
-      //{ path: 'counter', component: CounterComponent },
-      //{ path: 'mTopList', component: MTopListComponent },
       { path: 'movie', component: MovieComponent },
-      //{ path: 'dTopList', component: DirectorComponent }
     ])
   ],
   providers: [],
